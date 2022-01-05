a246b6b1a122aaf34ffb88ae2689f7601e1a9d48
andrewtsuk <66274064+andrewtsuk@users.noreply.github.com>
2022-01-04 19:42:47 -0800

    V1 Build

---
+++ Lab 2/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fl21/class/ee106a-acu/ros_workspaces/lab2/lab2/src
+# Build directory: /home/cc/ee106a/fl21/class/ee106a-acu/ros_workspaces/lab2/lab2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_chatter)
+subdirs(chatter)
+subdirs(lab2_turtlesim)
+subdirs(turtle_patrol)
---
+++ Lab 2/build/turtle_patrol/catkin_generated/turtle_patrol-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(turtle_patrol_MESSAGE_FILES "")
+set(turtle_patrol_SERVICE_FILES "/home/cc/ee106a/fl21/class/ee106a-acu/ros_workspaces/lab2/lab2/src/turtle_patrol/srv/Patrol.srv")
